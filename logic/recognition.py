45bbd3a92bca725ec67c640de2f325897da82a84
Rasm-P <rasmus.pra@gmail.com>
2020-04-08 17:49:43 +0200

    Added webcam mode

--- logic/recognition.py
+++ logic/recognition.py
@@ -29,6 +29,7 @@ def face_recognition_load():
                 known_faces.append(encoding)
                 known_names.append(name)
     __unknown_faces_processing(cv2_conf, frecog_conf, known_faces, known_names)
+    #__unknown_faces_processing_video(cv2_conf, frecog_conf, known_faces, known_names)
 
 
 def __unknown_faces_processing(cv2_conf, frecog_conf, known_faces, known_names):
@@ -76,3 +77,48 @@ def __unknown_faces_processing(cv2_conf, frecog_conf, known_faces, known_names):
         cv2.imshow(filename, image)
         cv2.waitKey(0)
         cv2.destroyWindow(filename)
+
+
+def __unknown_faces_processing_video(cv2_conf, frecog_conf, known_faces, known_names):
+    logger.info("Video: Processing unknown faces...")
+
+    while True:
+        ret, image = video.read()  
+        locations = face_recognition.face_locations(image, model=cv2_conf["Model"])
+        encodings = face_recognition.face_encodings(image, locations)
+        for face_encoding, face_location in zip(encodings, locations):
+            results = face_recognition.compare_faces(
+                known_faces, face_encoding, float(frecog_conf["Tolerance"])
+            )
+            match = None
+            if True in results:
+                match = known_names[results.index(True)]
+                logger.info(f"Video: Match found: {match}")
+                top_left = (face_location[3], face_location[0])
+                bottom_right = (face_location[1], face_location[2])
+                color = [0, 225, 0]
+                cv2.rectangle(
+                    image,
+                    top_left,
+                    bottom_right,
+                    color,
+                    int(cv2_conf["FrameThickness"]),
+                )
+                print("2")
+                top_left = (face_location[3], face_location[2])
+                bottom_right = (face_location[1], face_location[2] + 22)
+                cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+                cv2.putText(
+                    image,
+                    match,
+                    (face_location[3] + 10, face_location[2] + 15),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (200, 200, 200),
+                    int(cv2_conf["FontThickness"]),
+                )
+        print("3")
+        cv2.imshow("Video", image)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            cv2.destroyWindow(filename)
+            break
