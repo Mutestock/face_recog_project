fb1b37d6ae7a80c4ea7ce0a832044ce1f61f9db8
Henning Wiberg <doublenegative@protonmail.com>
2020-05-04 16:04:17 +0200

    pathing. Changes to tests, etc

---
+++ test/test_settings.py
@@ -0,0 +1,14 @@
+import unittest
+from settings.connector import DB_connection
+from settings.pathing import os_parse_path
+import os
+import cv2
+
+class Tests(unittest.TestCase):
+    def test_connection(self):
+        self.assertIsNotNone(DB_connection())
+    
+    def test_pathing(self):
+        cv2dir = os.path.dirname(cv2.__file__)
+        self.assertIsNotNone(os_parse_path(f"{cv2dir}\data\\haarcascade_fronta/face_default.xml"))
+
