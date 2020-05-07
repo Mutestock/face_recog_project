e0e4dfbdb9b3f55d025af293ac11fa48c4acbba1
Lukas-bjornvad <lukas.bjornvad@gmail.com>
2020-05-07 10:57:55 +0200

    added picture taking

--- Facial_tracking/OpenCV_Facial.py
+++ Facial_tracking/OpenCV_Facial.py
@@ -1,13 +1,13 @@
 import cv2
-import os
+import os, os.path
 from settings.pathing import os_parse_path
+import time
 
 def executor():
     #Loading cascades
-
-    #face_cascade = cv2.CascadeClassifier('C:/Python/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
-    #eye_cascade = cv2.CascadeClassifier('C:/Python/Lib/site-packages/cv2/data/haarcascade_eye.xml')
-    #smile_cascade = cv2.CascadeClassifier('C:/Python/Lib/site-packages/cv2/data/haarcascade_smile.xml')
+    face_cascade = cv2.CascadeClassifier('C:/Python/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('C:/Python/Lib/site-packages/cv2/data/haarcascade_eye.xml')
+    smile_cascade = cv2.CascadeClassifier('C:/Python/Lib/site-packages/cv2/data/haarcascade_smile.xml')
 
     #face_cascade_path =""
     #eye_cascade_path = ""
@@ -24,23 +24,31 @@ def executor():
     #    smile_cascade_path = f"{os.path.dirname(cv2.__file__)}\data\haarcascade_smile.xml"
     cv2dir = os.path.dirname(cv2.__file__)
 
-    face_cascade_path = os_parse_path(f"{cv2dir}\data\haarcascade_frontalface_default.xml")
-    eye_cascade_path = os_parse_path(f"{cv2dir}\data\haarcascade_eye.xml")
-    smile_cascade_path = os_parse_path(f"{cv2dir}\data\haarcascade_smile.xml")
+    #face_cascade_path = os_parse_path(f"{cv2dir}\data\haarcascade_frontalface_default.xml")
+    #eye_cascade_path = os_parse_path(f"{cv2dir}\data\haarcascade_eye.xml")
+    #smile_cascade_path = os_parse_path(f"{cv2dir}\data\haarcascade_smile.xml")
 
-    face_cascade = cv2.CascadeClassifier(face_cascade_path)
-    eye_sascade = cv2.CascadeClassifier(eye_cascade_path)
-    smile_cascade = cv2.CascadeClassifier(smile_cascade_path)
+    #face_cascade = cv2.CascadeClassifier(face_cascade_path)
+    #eye_sascade = cv2.CascadeClassifier(eye_cascade_path)
+    #smile_cascade = cv2.CascadeClassifier(smile_cascade_path)
 
     #Detection function
-
+   
     def detect(grey, frame):
+        path, dirs, files = next(os.walk("./pics"))
+        count = len(files)
+        print(count)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
             cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 255, 0), 2)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = frame[y:y+h, x:x+w]
-
+            if count < 8 :
+             time.sleep(0.5)
+             pic = "pics/pic"+str(count)+".jpg"
+             print(pic)
+             cv2.imwrite(pic, frame[y:y+h, x:x+w])
+             count += 1
             eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
             for (ex,ey,ew,eh) in eyes:
                 cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh),(0, 255, 0), 1)
@@ -54,7 +62,8 @@ def executor():
         #Face recognition with webcam
 
     cam = cv2.VideoCapture(0)
-
+    #boole = True
+    #count =0
     while True:
         _,frame=cam.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
