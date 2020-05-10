b383c1e3d12212db3dd115137cd4c0cd5ac77eca
Rasm-P <rasmus.pra@gmail.com>
2020-05-10 16:41:15 +0200

    Added more functionality to picture taking

--- Facial_tracking/OpenCV_Facial.py
+++ Facial_tracking/OpenCV_Facial.py
@@ -2,28 +2,15 @@ import cv2
 import os, os.path
 from settings.pathing import os_parse_path
 import time
+import tkinter as tk
+from tkinter import simpledialog
+import keyboard
+
+USER_INP = " "
+known_faces_path = "./facerec/known_faces"
 
 def executor():
-    #Loading cascades
-    #face_cascade = cv2.CascadeClassifier('C:/Python/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
-    #eye_cascade = cv2.CascadeClassifier('C:/Python/Lib/site-packages/cv2/data/haarcascade_eye.xml')
-    #smile_cascade = cv2.CascadeClassifier('C:/Python/Lib/site-packages/cv2/data/haarcascade_smile.xml')
-
-    #face_cascade_path =""
-    #eye_cascade_path = ""
-    #smile_cascade_path = ""
-#
-    #if(platform.system() is 'Windows'):
-    #    face_cascade_path = f"{os.path.dirname(cv2.__file__)}/data/haarcascade_frontalface_default.xml"
-    #    eye_cascade_path = f"{os.path.dirname(cv2.__file__)}/data/haarcascade_eye.xml"
-    #    smile_cascade_path = f"{os.path.dirname(cv2.__file__)}/data/haarcascade_smile.xml"
-#
-    #else:
-    #    face_cascade_path = f"{os.path.dirname(cv2.__file__)}\data\haarcascade_frontalface_default.xml"
-    #    eye_cascade_path = f"{os.path.dirname(cv2.__file__)}\data\haarcascade_eye.xml"
-    #    smile_cascade_path = f"{os.path.dirname(cv2.__file__)}\data\haarcascade_smile.xml"
-
-    
+    #Loading cascades    
     cv2dir = os.path.dirname(cv2.__file__)
 
     face_cascade_path = os_parse_path(f"{cv2dir}\data\haarcascade_frontalface_default.xml")
@@ -31,26 +18,37 @@ def executor():
     smile_cascade_path = os_parse_path(f"{cv2dir}\data\haarcascade_smile.xml")
 
     face_cascade = cv2.CascadeClassifier(face_cascade_path)
-    eye_sascade = cv2.CascadeClassifier(eye_cascade_path)
+    eye_cascade = cv2.CascadeClassifier(eye_cascade_path)
     smile_cascade = cv2.CascadeClassifier(smile_cascade_path)
 
+    ROOT = tk.Tk()
+    ROOT.withdraw()
+
     #Detection function
-   
     def detect(grey, frame):
-        path, dirs, files = next(os.walk("./pics"))
+        global known_faces_path
+        global USER_INP
+        path, dirs, files = next(os.walk(known_faces_path))
         count = len(files)
         print(count)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
             cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 255, 0), 2)
             roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
-            if count < 8 :
-             time.sleep(0.5)
-             pic = "pics/pic"+str(count)+".jpg"
-             print(pic)
-             cv2.imwrite(pic, frame[y:y+h, x:x+w])
-             count += 1
+            roi_color = frame[y:y+h, x:x+w]    
+            if keyboard.is_pressed('r'):
+                if count < 10:
+                    if (USER_INP == " "):
+                        USER_INP = simpledialog.askstring(title="Name", prompt="What's your Name?:")
+                    #time.sleep(0.5)
+                    pathFace = "facerec/known_faces/" + USER_INP + "/"
+                    if not os.path.exists(pathFace):
+                        os.makedirs(pathFace)
+                        known_faces_path = pathFace
+                    pic = pathFace +  USER_INP + str(count)+".jpg"
+                    print(pic)
+                    cv2.imwrite(pic, frame[y:y+h, x:x+w])
+                    count += 1
             eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
             for (ex,ey,ew,eh) in eyes:
                 cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh),(0, 255, 0), 1)
@@ -61,8 +59,8 @@ def executor():
 
         return frame
 
-        #Face recognition with webcam
 
+    #Face recognition with webcam
     cam = cv2.VideoCapture(0)
     #boole = True
     #count =0
@@ -79,6 +77,6 @@ def executor():
 
 
 if(__name__ == '__main__'):
-    cv2dirtest = os.path.dirname(cv2.__file__)
+    cvf2dirtest = os.path.dirname(cv2.__file__)
     face_cascade_pathtest = os_parse_path(f"{cvf2dirtest}\data\haarcascade_frontalface_default.xml")
     print(face_cascade_pathtest)
